b446bd66575f2e86d305833cc481f28736c268d0
Manomay Kagalkar <manomay111@gmail.com>
2023-01-19 23:40:45 +0530

    added build folder

---
+++ tedxvitbhopal/build/precache-manifest.f6f3db8c3b79d7c551a5ff22d523708f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0559e1cb8e132626d6a4",
+    "url": "/static/css/main.3c3f3696.chunk.css"
+  },
+  {
+    "revision": "0559e1cb8e132626d6a4",
+    "url": "/static/js/main.0559e1cb.chunk.js"
+  },
+  {
+    "revision": "983e6ea30e3f24840747",
+    "url": "/static/js/1.983e6ea3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6aaea9f2761a13a9c413fdc442f27fec",
+    "url": "/static/media/logo-white.6aaea9f2.png"
+  },
+  {
+    "revision": "5e50007c58f3521b7fd6470e6af616c6",
+    "url": "/static/media/twitter.5e50007c.png"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/static/media/instagram.5c570427.png"
+  },
+  {
+    "revision": "e87e882c2659b4fadf8fad828894c749",
+    "url": "/static/media/gmaps.e87e882c.png"
+  },
+  {
+    "revision": "ac7174b692fcfd200570f75ace119301",
+    "url": "/static/media/x-neon-sign.ac7174b6.jpg"
+  },
+  {
+    "revision": "a000b8dd8d9124534cd2472638f6ac02",
+    "url": "/index.html"
+  }
+];
